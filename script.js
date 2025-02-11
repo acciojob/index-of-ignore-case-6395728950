e77085577901349f1beadd558eda06f2e2508bd8
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-02-11 23:35:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+
+	for(let i=0;i<s1.length;i++){
+		let j =i;
+		let  k=0;
+		while( k <s2.length && i<s1.length && s1[i].toLowerCase()==s2[k].toLowerCase() ){
+			 k++;
+			i++;
+		}
+		if(k==s2.length){
+			return j;
+		}
+	}
+	return -1;
 }
 
 // Please do not change the code below
